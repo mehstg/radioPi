6ef8d09f4389f6a8297b3fdfb6a4e2b90424e864
Paul Braham <mehstg@gmail.com>
2013-08-20 15:33:27 +0000

    Playing with classes

---
+++ ScreenAccess.py
@@ -0,0 +1,10 @@
+# RadioPi - Paul Braham
+# Screen Access Class
+# Used by the radio application to interface with the Phi-Prompt Scren Controller.
+
+import serial
+
+class ScreenAccess:
+	def __init__(testString):
+
+		return testString
---
+++ radioPy.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+# RadioPi - An Internet Radio application for the Raspberry Pi
+# Paul Braham 2013
+# Main Class
+
+from ScreenAccess import ScreenAccess
+
+screenAccess = ScreenAccess()
+
+def main ():
+	print "Hello World"
+	screenAccess = ScreenAccess("Test")
+
+
+
+
+#Invoke main function
+if __name__ == "__main__":
+    main()
